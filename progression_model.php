0c0fc6b15d3b4614cfaeb98e395685d224729cd0
Khalessi800 <tahirijihane467@gmail.com>
2025-01-13 22:53:29 +0100

    Add files via upload

---
+++ progression_model.php
@@ -0,0 +1,30 @@
+<?php
+class Progression{
+    private $conn;
+
+    public $id;
+    public $participant_id;
+    public $formation_id;
+    public $score_quiz;
+    public $termine;
+
+    public function __construct($conn){
+        $this->conn=$conn;
+    }
+
+    public function addProgression($participant_id,$formation_id,$score_quiz,$termine=false){
+        try{
+            $query="INSERT INTO Progression(id,participant_id,formation_id,score_quiz,termine) values(?,?,?,?)";
+            $exec=$this->conn->prepare($query);
+            $exec->execute([$participant_id,$formation_id,$score_quiz,$termine]);
+            echo "preogression assigned!";
+
+
+        }catch(PDOException $e){
+            echo "progression not successfully assigned ".$e->getMessage();
+        }
+ 
+    }
+
+}
+?>
